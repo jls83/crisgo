4e9b5b0d07af7ffb41ace4c6119df7c7fb1b10dd
jls83 <josephlsanders@gmail.com>
2020-05-23 18:24:08 -0400

    Add checking to config constructor

--- config/config.go
+++ config/config.go
@@ -8,12 +8,16 @@ import (
     "gopkg.in/yaml.v2"
 )
 
+const DEFAULT_PORT_NUMBER = 8080
+
+const DEFAULT_SQLITE_FILE_PATH = "crisgo.db"
+const DEFAULT_SQLITE_TABLE_NAME = "shortened_urls"
+
 type CrisgoConfig struct {
     Tablename           string  `yaml:"tablename"`
     DatabaseFilePath    string  `yaml:"database_file_path"`
     PortNumber          int     `yaml:"port_number"`
 }
-
 
 func NewCrisgoConfig(filepath string) *CrisgoConfig {
     config := CrisgoConfig{}
@@ -30,6 +34,22 @@ func NewCrisgoConfig(filepath string) *CrisgoConfig {
     err = yaml.Unmarshal(fileContents, &config)
     if err != nil {
         log.Fatal(err)
+    }
+
+    // Check our values
+    tablename := DEFAULT_SQLITE_TABLE_NAME
+    if reflect.ValueOf(config.Tablename).IsZero() {
+        config.Tablename = DEFAULT_SQLITE_TABLE_NAME
+    }
+
+    // Get file_path
+    if reflect.ValueOf(config.DatabaseFilePath).IsZero() {
+        config.DatabaseFilePath = DEFAULT_SQLITE_FILE_PATH
+    }
+
+    // Get port_number
+    if reflect.ValueOf(config.PortNumber).IsZero() {
+        config.PortNumber = DEFAULT_PORT_NUMBER
     }
 
     return &config
